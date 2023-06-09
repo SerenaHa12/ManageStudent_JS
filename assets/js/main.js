d5f4ec72e6dfe7f289f17a1880a95c1eb9433adc
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-10 02:45:46 +0700

    fix console.log student object

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,13 @@ function save(){
     let phoneNumber = document.getElementById('phone-number').value;
     let address = document.getElementById('address').value;
     let gender = '';
+    let student = {
+        fullname: fullname,
+        email: email,
+        phoneNumber: phoneNumber,
+        address: address,
+        gender: gender
+    };
     if (document.getElementById('male').checked) {
         gender = document.getElementById('male').value;
     } else if(document.getElementById('female').checked) {
@@ -16,72 +23,63 @@ function save(){
     // console.log(fullname, email, phoneNumber, address, gender);
     // name validated
     if (_.isEmpty(fullname)){
-        fullname = '';
+        // fullname = '';
         document.getElementById("fullname-error").innerHTML = "* Please enter a fullname";
     } else if(fullname.trim().length <= 2) {
-        fullname = '';
+        // fullname = '';
         document.getElementById('fullname-error').innerHTML = '* Please enter more than 2 characters';
     } else if(fullname.trim().length > 50) {
-        fullname = '';
+        // fullname = '';
         document.getElementById('fullname-error').innerHTML = '* Please enter less than 50 characters';
     }
     else{
-        fullname = '';
+        // fullname = '';
         document.getElementById('fullname-error').innerHTML ='';
     } 
 
     // email validated
     if (_.isEmpty(email)){
-        email = '';
+        // email = '';
         document.getElementById("email-error").innerHTML = "* Please enter a email";
     } else if(!emailIsValid(email)){
-        email = '';
+        // email = '';
         document.getElementById('email-error').innerHTML = "* Please using right valid email form";
     } else{
-        email = '';
+        // email = '';
         document.getElementById('email-error').innerHTML ='';
     }
 
     // phone validated
     if (_.isEmpty(phoneNumber)){
-        phoneNumber = '';
+        // phoneNumber = '';
         document.getElementById("number-error").innerHTML = "* Please enter a phone number";
     } else if(fullname.trim().length > 10) {
-        phoneNumber = '';
+        // phoneNumber = '';
         document.getElementById('number-error').innerHTML = '* Please enter less than 10 numbers';
     } else{
-        phoneNumber = '';
+        // phoneNumber = '';
         document.getElementById('number-error').innerHTML ='';
     }
 
     // address validated
     if (_.isEmpty(address)){
-        address = '';
+        // address = '';
         document.getElementById("address-error").innerHTML = "* Please enter a address";
     } else{
-        address = '';
+        // address = '';
         document.getElementById('address-error').innerHTML ='';
     }
 
     // gender validated
     if (_.isEmpty(gender)){
-        gender = '';
+        // gender = '';
         document.getElementById('gender-error').innerHTML = '* Please select your gender';
     } else{
-        gender = '';
+        // gender = '';
         document.getElementById('gender-error').innerHTML ='';
     }
 
     if (fullname && email && phoneNumber && address && gender) {
-        // console.log(fullname, email, phoneNumber, address, gender);
-        console.log(fullname, email, phoneNumber, address, gender);
-        // let student = {
-        //     fullname: fullname,
-        //     email: email,
-        //     phoneNumber: phoneNumber,
-        //     address: address,
-        //     gender: gender
-        // };
-        // console.log(student);
+        console.log(student);
     }
 }
