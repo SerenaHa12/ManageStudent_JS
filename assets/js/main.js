c689bd4c72a9a3410773760028624b1ffe0b9ea4
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-10 03:46:40 +0700

    done localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,50 +23,51 @@ function save() {
     }
 
     // Kiểm tra hợp lệ cho tên
-    if (_.isEmpty(fullname)) {
-        document.getElementById("fullname-error").innerHTML = "* Vui lòng nhập họ tên";
+    if (fullname.trim().length === 0) {
+        document.getElementById("fullname-error").innerHTML = "* Please enter a full name";
     } else if (fullname.trim().length <= 2) {
-        document.getElementById('fullname-error').innerHTML = '* Vui lòng nhập hơn 2 ký tự';
+        document.getElementById('fullname-error').innerHTML = '* Please enter more than 2 characters';
     } else if (fullname.trim().length > 50) {
-        document.getElementById('fullname-error').innerHTML = '* Vui lòng nhập ít hơn 50 ký tự';
+        document.getElementById('fullname-error').innerHTML = '* Please enter less than 50 characters';
     } else {
         document.getElementById('fullname-error').innerHTML = '';
     }
 
     // Kiểm tra hợp lệ cho email
-    if (_.isEmpty(email)) {
-        document.getElementById("email-error").innerHTML = "* Vui lòng nhập email";
+    if (email.trim().length === 0) {
+        document.getElementById("email-error").innerHTML = "* Please enter your email address";
     } else if (!emailIsValid(email)) {
-        document.getElementById('email-error').innerHTML = "* Vui lòng sử dụng đúng định dạng email";
+        document.getElementById('email-error').innerHTML = "* Please enter right email form";
     } else {
         document.getElementById('email-error').innerHTML = '';
     }
 
     // Kiểm tra hợp lệ cho số điện thoại
-    if (_.isEmpty(phoneNumber)) {
-        document.getElementById("number-error").innerHTML = "* Vui lòng nhập số điện thoại";
+    if (phoneNumber.trim().length === 0) {
+        document.getElementById("number-error").innerHTML = "* Please enter your phone number";
     } else if (phoneNumber.trim().length > 10) {
-        document.getElementById('number-error').innerHTML = '* Vui lòng nhập ít hơn 10 chữ số';
+        document.getElementById('number-error').innerHTML = '* Please enter less then 10 numbers';
     } else {
         document.getElementById('number-error').innerHTML = '';
     }
 
     // Kiểm tra hợp lệ cho địa chỉ
-    if (_.isEmpty(address)) {
-        document.getElementById("address-error").innerHTML = "* Vui lòng nhập địa chỉ";
+    if (address.trim().length === 0) {
+        document.getElementById("address-error").innerHTML = "* Please enter your address";
     } else {
         document.getElementById('address-error').innerHTML = '';
     }
 
     // Kiểm tra hợp lệ cho giới tính
-    if (_.isEmpty(gender)) {
-        document.getElementById('gender-error').innerHTML = '* Vui lòng chọn giới tính';
+    if (gender.trim().length === 0) {
+        document.getElementById('gender-error').innerHTML = '* Please select your gender';
     } else {
         document.getElementById('gender-error').innerHTML = '';
     }
 
-    if (fullname !== '' && email !== '' && phoneNumber !== '' && address !== '' && gender !== '') {
-        let students = [];
+    if (fullname.trim().length > 0 && email.trim().length > 0 && phoneNumber.trim().length > 0 && address.trim().length > 0 && gender.trim().length > 0) {
+        let students = localStorage.getItem('students') ? JSON.parse(localStorage.getItem('students')) : [];
+
         students.push({
             fullname: fullname,
             email: email,
@@ -75,6 +76,10 @@ function save() {
             gender: gender
         });
 
+        localStorage.setItem('students', JSON.stringify(students));
+
+        renderListStudent();
+
         let tableContent = `
             <tr>
                 <td>#</td>
@@ -82,14 +87,12 @@ function save() {
                 <td>Email</td>
                 <td>Phone Number</td>
                 <td>Address</td>
-                <td>Gender</td>
                 <td>Action</td>
             </tr>
         `;
 
         students.forEach((student, index) => {
             index++;
-            let genderLabel = student.gender === 1 ? 'Male':'Female';
             tableContent += `
                 <tr>
                     <td>${index}</td>
@@ -97,7 +100,6 @@ function save() {
                     <td>${student.email}</td>
                     <td>${student.phoneNumber}</td>
                     <td>${student.address}</td>
-                    <td>${genderLabel}</td>
                     <td>
                         <a href="#">Edit</a> | <a href="#">Delete</a>
                     </td>
@@ -106,4 +108,42 @@ function save() {
 
         document.getElementById('grid-students').innerHTML = tableContent;
     }
+}
+
+function renderListStudent() {
+    let students = localStorage.getItem('students') ? JSON.parse(localStorage.getItem('students')) : [];
+    if (students.length === 0) {
+        document.getElementById('list-student').style.display = 'none';
+        return false;
+    }
+    document.getElementById('list-student').style.display = 'block';
+    let tableContent = `
+        <tr>
+            <td>#</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone Number</td>
+            <td>Address</td>
+            <td>Gender</td>
+            <td>Action</td>
+        </tr>
+    `;
+    students.forEach((student, index) => {
+        index++;
+        let genderLabel = student.gender === 'male' ? 'Male' : 'Female';
+        tableContent += `
+            <tr>
+                <td>${index}</td>
+                <td>${student.fullname}</td>
+                <td>${student.email}</td>
+                <td>${student.phoneNumber}</td>
+                <td>${student.address}</td>
+                <td>${genderLabel}</td>
+                <td>
+                    <a href="#">Edit</a> | <a href="#">Delete</a>
+                </td>
+            </tr>`;
+    });
+
+    document.getElementById('grid-students').innerHTML = tableContent;
 }
