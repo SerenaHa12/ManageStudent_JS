4dbb3b4872e8da7d531515893cf9e177a5c6d17d
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-10 03:18:36 +0700

    save student information

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
-function emailIsValid(email){
+function emailIsValid(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
-function save(){
+
+function save() {
     let fullname = document.getElementById('fullname').value;
     let email = document.getElementById('email').value;
     let phoneNumber = document.getElementById('phone-number').value;
@@ -14,72 +15,95 @@ function save(){
         address: address,
         gender: gender
     };
+
     if (document.getElementById('male').checked) {
         gender = document.getElementById('male').value;
-    } else if(document.getElementById('female').checked) {
+    } else if (document.getElementById('female').checked) {
         gender = document.getElementById('female').value;
     }
 
-    // console.log(fullname, email, phoneNumber, address, gender);
-    // name validated
-    if (_.isEmpty(fullname)){
-        // fullname = '';
-        document.getElementById("fullname-error").innerHTML = "* Please enter a fullname";
-    } else if(fullname.trim().length <= 2) {
-        // fullname = '';
-        document.getElementById('fullname-error').innerHTML = '* Please enter more than 2 characters';
-    } else if(fullname.trim().length > 50) {
-        // fullname = '';
-        document.getElementById('fullname-error').innerHTML = '* Please enter less than 50 characters';
-    }
-    else{
-        // fullname = '';
-        document.getElementById('fullname-error').innerHTML ='';
-    } 
-
-    // email validated
-    if (_.isEmpty(email)){
-        // email = '';
-        document.getElementById("email-error").innerHTML = "* Please enter a email";
-    } else if(!emailIsValid(email)){
-        // email = '';
-        document.getElementById('email-error').innerHTML = "* Please using right valid email form";
-    } else{
-        // email = '';
-        document.getElementById('email-error').innerHTML ='';
+    // Kiểm tra hợp lệ cho tên
+    if (_.isEmpty(fullname)) {
+        document.getElementById("fullname-error").innerHTML = "* Vui lòng nhập họ tên";
+    } else if (fullname.trim().length <= 2) {
+        document.getElementById('fullname-error').innerHTML = '* Vui lòng nhập hơn 2 ký tự';
+    } else if (fullname.trim().length > 50) {
+        document.getElementById('fullname-error').innerHTML = '* Vui lòng nhập ít hơn 50 ký tự';
+    } else {
+        document.getElementById('fullname-error').innerHTML = '';
     }
 
-    // phone validated
-    if (_.isEmpty(phoneNumber)){
-        // phoneNumber = '';
-        document.getElementById("number-error").innerHTML = "* Please enter a phone number";
-    } else if(fullname.trim().length > 10) {
-        // phoneNumber = '';
-        document.getElementById('number-error').innerHTML = '* Please enter less than 10 numbers';
-    } else{
-        // phoneNumber = '';
-        document.getElementById('number-error').innerHTML ='';
+    // Kiểm tra hợp lệ cho email
+    if (_.isEmpty(email)) {
+        document.getElementById("email-error").innerHTML = "* Vui lòng nhập email";
+    } else if (!emailIsValid(email)) {
+        document.getElementById('email-error').innerHTML = "* Vui lòng sử dụng đúng định dạng email";
+    } else {
+        document.getElementById('email-error').innerHTML = '';
     }
 
-    // address validated
-    if (_.isEmpty(address)){
-        // address = '';
-        document.getElementById("address-error").innerHTML = "* Please enter a address";
-    } else{
-        // address = '';
-        document.getElementById('address-error').innerHTML ='';
+    // Kiểm tra hợp lệ cho số điện thoại
+    if (_.isEmpty(phoneNumber)) {
+        document.getElementById("number-error").innerHTML = "* Vui lòng nhập số điện thoại";
+    } else if (phoneNumber.trim().length > 10) {
+        document.getElementById('number-error').innerHTML = '* Vui lòng nhập ít hơn 10 chữ số';
+    } else {
+        document.getElementById('number-error').innerHTML = '';
     }
 
-    // gender validated
-    if (_.isEmpty(gender)){
-        // gender = '';
-        document.getElementById('gender-error').innerHTML = '* Please select your gender';
-    } else{
-        // gender = '';
-        document.getElementById('gender-error').innerHTML ='';
+    // Kiểm tra hợp lệ cho địa chỉ
+    if (_.isEmpty(address)) {
+        document.getElementById("address-error").innerHTML = "* Vui lòng nhập địa chỉ";
+    } else {
+        document.getElementById('address-error').innerHTML = '';
     }
 
-    if (fullname && email && phoneNumber && address && gender) {
-        console.log(student);
+    // Kiểm tra hợp lệ cho giới tính
+    if (_.isEmpty(gender)) {
+        document.getElementById('gender-error').innerHTML = '* Vui lòng chọn giới tính';
+    } else {
+        document.getElementById('gender-error').innerHTML = '';
+    }
+
+    if (fullname !== '' && email !== '' && phoneNumber !== '' && address !== '' && gender !== '') {
+        let students = [];
+        students.push({
+            fullname: fullname,
+            email: email,
+            phoneNumber: phoneNumber,
+            address: address,
+            gender: gender
+        });
+
+        let tableContent = `
+            <tr>
+                <td>#</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone Number</td>
+                <td>Address</td>
+                <td>Gender</td>
+                <td>Action</td>
+            </tr>
+        `;
+
+        students.forEach((student, index) => {
+            index++;
+            let genderLabel = student.gender === 1 ? 'Male':'Female';
+            tableContent += `
+                <tr>
+                    <td>${index}</td>
+                    <td>${student.fullname}</td>
+                    <td>${student.email}</td>
+                    <td>${student.phoneNumber}</td>
+                    <td>${student.address}</td>
+                    <td>${genderLabel}</td>
+                    <td>
+                        <a href="#">Edit</a> | <a href="#">Delete</a>
+                    </td>
+                </tr>`;
+        });
+
+        document.getElementById('grid-students').innerHTML = tableContent;
     }
 }
